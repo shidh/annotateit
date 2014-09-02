3197339279459e0230a8ecbbb5d35e5d20a7f808
Allen Shi <shidenghui@gmail.com>
2014-09-02 17:36:58 +0200

    make run.sh more friendly

--- run.sh
+++ run.sh
@@ -5,6 +5,37 @@
 #Please run bootstrap.py before you run me, *IF* it is the *FIRST* time
 #you start annotator.
 
+#help information
+usage()
+{
+   echo " USAGE: $0  [init|start]"
+   echo ""
+   echo " OPTIONS: "
+   echo " init: for initialization when you run annotator first time"
+   echo " start: start annotator application"
+   exit 1
+}
+
+#Receiving the value of the parameter
+start=1;
+while [ $# -ge 0 ]
+  do
+    case $1 in
+              init) let start=0;
+                break
+                ;;
+              start) let start=1;
+                break
+                ;;
+              -help|-h|--help)usage
+                break
+                ;;
+              *)usage
+                break
+                ;;
+  esac
+  done
+
 #init some environment variables which are needed by annotator
 export SECRET_KEY="DibR3IsUqANtD6aBnMz9y+4jHubdOOYmGMsd8m7IEcI="
 export RECAPTCHA_PUBLIC_KEY='6Lc0U-MSAAAAAFBFR8WI19Jj5bcVdYXg45kCvW0m'
@@ -15,17 +46,25 @@ export AUTH_ON='Ture'
 export AUTHZ_ON='Ture'
 
 #create a new virtual env of Python
-virtualenv pyenv
+if [ $start = 0 ];then
+  virtualenv pyenv
+fi
 #active it and install required dependencies, see setup.py and requirements.txt
-. pyenv/bin/activate
-pip install -r requirements.txt
+if [ -d "pyenv" ]; then
+  . pyenv/bin/activate
+else
+  echo "Please run 'sh run.sh init' first"
+  exit 0
+fi
 
-
+if [ $start = 0 ];then
+  pip install -r requirements.txt
+  python bootstrap.py
+fi
 #consumer = readlift
 #Primary consumer secret: 4c4e8dd4-96e2-4f22-9d95-b4f9c712d70f
 
 #start web app
 python run.py "$@"
-
 
 
